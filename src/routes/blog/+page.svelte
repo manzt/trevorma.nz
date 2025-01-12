d12cc16da3e15273c4d7724593664d47e75662d3
Trevor Manz <trevor.j.manz@gmail.com>
2025-01-12 13:05:07 -0500

    Add blog route

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import Typewriter from "../Typewriter.svelte";
+
+let less = $state(true);
+</script>
+
+<svelte:head>
+	<title>blog</title>
+	<meta name="description" content="trevor's blog" />
+</svelte:head>
+
+<h1 class="text-3xl font-semibold">
+	<Typewriter text="blog" />trevor manz
+</h1>
