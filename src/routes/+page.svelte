4e67d263c25f927f419a480f086fb7fb6e0816b5
Trevor Manz <trevor.j.manz@gmail.com>
2025-03-26 08:07:44 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,10 +10,10 @@
 	<p>i make tools to help humans answer questions with data.</p>
 
 	<p>
-		i completed my phd at harvard in the <a
+		i recently completed my phd at harvard in the <a
 			target="_blank"
 			href="https://hidivelab.org">hidive lab</a
-		>.
+		>, where i'm now a postdoc.
 	</p>
 
 	<p>
