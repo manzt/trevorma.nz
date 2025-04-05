fb898ea752bd01af86a5dab84e5f631d025c8656
Trevor Manz <trevor.j.manz@gmail.com>
2025-04-05 15:54:49 -0400

    fix up styles

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -10,37 +10,30 @@ let { data }: { data: PageData } = $props();
 	<meta name="description" content="trevor's blog" />
 </svelte:head>
 
-<section>
-	<ul class="divide-y divide-gray-200 dark:divide-gray-200/5">
-		{#each [...data.posts, ...data.posts] as { slug, frontmatter }}
-			<li class="py-6">
-				<article
-					class="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline"
-				>
-					<dl>
+<section class="mx-1 text-gray-700 dark:text-gray-300">
+	<ul class="space-y-4 divide-y divide-gray-200 dark:divide-gray-600">
+		{#each data.posts as { slug, frontmatter }}
+			<li class="pb-4">
+				<article>
+					<h2 class="text-xl font-bold">
+						<a
+							class="hover:underline underline-offset-2"
+							href={`/blog/${slug}`}>{frontmatter.title}</a
+						>
+					</h2>
+					<dl class="mt-1">
 						<dt class="sr-only">Published on</dt>
-						<dd class="text-sm text-gray-500 dark:text-gray-300">
+						<dd class="text-sm text-gray-400 dark:text-gray-500">
 							<time datetime={frontmatter.date.toString()}
 								>{utils.formatDate(frontmatter.date)}</time
 							>
 						</dd>
 					</dl>
-					<div class="space-y-2 xl:col-span-3">
-						<div class="space-y-2">
-							<h2 class="text-lg leading-8 font-bold">
-								<a
-									class="text-gray-900 dark:text-white hover:underline underline-offset-2"
-									href={`/blog/${slug}`}
-									>{frontmatter.title}</a
-								>
-							</h2>
-							<div
-								class="max-w-none text-gray-500 dark:text-gray-300"
-							>
-								<p>{frontmatter.excerpt}</p>
-							</div>
-						</div>
-					</div>
+					<p
+						class="mt-6 prose max-w-none text-gray-500 dark:text-gray-400"
+					>
+						{frontmatter.excerpt}
+					</p>
 				</article>
 			</li>
 		{/each}
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
 import * as utils from "$lib/utils.ts";
 import type { PageData } from "./$types.ts";
-
 let { data }: { data: PageData } = $props();
 let { title, frontmatter, content } = data;
 </script>
 
 <section class="prose mx-1">
-	<h1 class="text-xl">{title}</h1>
-	<time class="text-gray-400 dark:text-gray-500">{utils.formatDate(frontmatter.date)}</time>
+	<div class="flex justify-between items-start w-full">
+		<div>
+			<h1 class="text-xl">{title}</h1>
+			<time class="text-gray-400 dark:text-gray-500"
+				>{utils.formatDate(frontmatter.date)}</time
+			>
+		</div>
+		<a
+			href="/blog"
+			class="inline-block mb-4 text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-200"
+		>
+			← back
+		</a>
+	</div>
 	<article>
 		{@html content}
 	</article>
